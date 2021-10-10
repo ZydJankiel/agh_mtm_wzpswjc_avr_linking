3dfc92c2adae6ee88a8dd3034990e2533ed97936
Piotr Wilk-Juraszek <pwj1234567890@gmail.com>
2021-10-10 22:33:16 +0200

    Added AVR Toolchain to PATH variable, and generated new version.txt

---
+++ zad2/main1.c
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+
+void led_init(void){
+   DDRB = 0x2;
+   PORTB = 0x2;
+   DDRD = 0xFF;
+}
+
+void led_on(void){
+   PORTD = 0xFF;
+}
+
+void led_off(void){
+   PORTD = 0x0F;
+}
+
+char cLedState = 1;
+
+int main(void){
+  led_init();
+	
+  while(1){
+    if(cLedState == 1){
+      led_on();
+    }
+    else{
+      led_off();
+    }
+    cLedState = ~cLedState;
+  }
+}
